5bc08b2843a7f1e41ac28e751ba0f0157d085de1
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-12-30 12:04:06 +0300

    feat: add connection to mongodb

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 const express = require('express');
 const config = require('config');
-// const path = require('path');
-// const cookieParser = require('cookie-parser');
-// const logger = require('morgan');
-//
-// const indexRouter = require('./routes/index');
-// const usersRouter = require('./routes/users');
+const mongoose = require('mongoose');
 
 const app = express();
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({extended: false}));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-//
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
 const PORT = config.get('port') || 5000;
 
-app.listen(PORT, () => {
-  console.log(`App has been started on port: ${PORT}...`);
-});
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongodb'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
 
-module.exports = app;
+    app.listen(PORT, () => console.log(`App has been started on port: ${PORT}...`));
+  } catch (e) {
+    console.log('Server error', e.message);
+    process.exit(1);
+  }
+}
+
+start();
