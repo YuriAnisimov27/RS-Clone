442c081bc8d68f1132d658e9cc0dc75df114998a
Aleks <lexamart-1708@gmail.com>
2021-01-21 15:45:43 +0300

    refactor: delete unusefull components

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,9 @@ import {BrowserRouter} from 'react-router-dom';
 import {useRoutes} from './routes';
 import {useAuth} from './hooks/auth.hook';
 import {Navbar} from './components/Navbar';
-import {Footer} from './components/Footer';
+import {Footer} from './pages/authPageElements/Footer';
 import {Loader} from './components/Loader';
-import {Header} from './components/Header';
+import {Header} from './pages/authPageElements/Header';
 import 'materialize-css';
 import './App.css';
 
