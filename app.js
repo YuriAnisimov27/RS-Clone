79b49a8ab22d51b40415dbe93958eda58112efae
YuriAnisimov27 <yurianisimov27@gmail.com>
2021-01-25 21:34:54 +0300

    refactor: removed draft files

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const app = express();
 
 app.use(express.json({extended: true}));
 app.use('/api/auth', require('./routes/auth.routes'));
-app.use('/api/admin', require('./routes/admin.routes'));
 app.use(cors());
 
 app.use(function (req, res, next) {
