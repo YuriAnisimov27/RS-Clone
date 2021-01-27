79f5a4d29744bddfe1fce1a02607738e9a50b6bb
Aleks <lexamart-1708@gmail.com>
2021-01-27 16:47:29 +0300

    fix: off game and solve errors

--- client/src/pages/GamePage/GamePage.js
+++ client/src/pages/GamePage/GamePage.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./GamePage.css";
-import PlayGame from "../../game/game";
+// import PlayGame from "../../game/game";
 
 const GamePage = () => <div id="game-wrapper" />;
 
-const gameStart = new PlayGame();
-gameStart.init();
+// const gameStart = new PlayGame();
+// gameStart.init();
 
 export default GamePage;
