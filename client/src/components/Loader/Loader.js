0272e57347db19fa1f295350661c0fb1972d27cc
YuriAnisimov27 <yurianisimov27@gmail.com>
2021-01-30 13:21:42 +0300

    feat: add styles to Loader

--- client/src/components/Loader/Loader.js
+++ client/src/components/Loader/Loader.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./Loader.css";
 
 const Loader = () => (
-  <div className="loadingio-spinner-pulse-txs6ijw9v5">
-    <div className="ldio-lz2fs42hqmm">
-      <div />
-      <div />
-      <div />
+  <div className="d-flex">
+    <div className="loadingio-spinner-pulse-txs6ijw9v5">
+      <div className="ldio-lz2fs42hqmm">
+        <div />
+        <div />
+        <div />
+      </div>
     </div>
   </div>
 );
